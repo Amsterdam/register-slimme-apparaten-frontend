86bc87b7733cbbec97494fda1d9d31ad218476f8
Robert Lodder <r.lodder@amsterdam.nl>
2018-12-21 14:52:01 +0100

    OP-508: Update disclaimer text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,14 +10,13 @@ function Footer() {
           <footer className="footer-component">
             <h3 className="c-footer__heading">Disclaimer</h3>
             <ul>
-              <li>Het register is niet volledig. Registratie van slimme apparaten in het register is niet verplicht, maar wordt wel gevraagd.</li>
+              <li>Het register biedt geen compleet overzicht van alle slimme apparaten. Registratie van slimme apparaten in het register is niet verplicht, maar wordt wel gevraagd.</li>
               <li>De &#39;slimme apparaten&#39; in het register zijn van diverse organisaties.</li>
               <li>De gegevens van de eigenaar van het slimme apparaat worden niet getoond i.v.m. privacy.</li>
               <li>De eigenaar bepaalt of hij reageert; hij is dat niet verplicht.</li>
-              <li>De mail wordt niet gearchiveerd in het register. Ook uw gegevens worden niet geregistreerd in het register.</li>
+              <li>Uw mail wordt niet gearchiveerd in het register. Ook uw persoonlijke gegevens worden niet geregistreerd in het register.</li>
               <li>De gegevens van de apparaten in het register worden overgenomen van de eigenaar. Die is verantwoordelijk voor de juistheid en volledigheid van de gegevens.</li>
-              <li>De maker van de software, OIS van de gemeente Amsterdam is niet verantwoordelijk voor de kwaliteit, het functioneren en de inhoud van het register.</li>
-              <li>De Gemeente Amsterdam, OIS kan niet aansprakelijk worden gesteld voor de juistheid, volledigheid en actualiteit van de website. De Gemeente Amsterdam, OIS kan in het bijzonder niet aansprakelijk worden gesteld voor eventuele schade of consequenties ontstaan door direct of indirect gebruik van de inhoud van de website.</li>
+              <li>OIS, onderdeel van Gemeente Amsterdam, kan niet aansprakelijk worden gesteld voor de juistheid, volledigheid en actualiteit van de website. De Gemeente Amsterdam, OIS kan in het bijzonder niet aansprakelijk worden gesteld voor eventuele schade of consequenties ontstaan door direct of indirect gebruik van de inhoud van de website.</li>
             </ul>
           </footer>
         </div>
