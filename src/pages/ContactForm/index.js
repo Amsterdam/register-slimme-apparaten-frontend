4ed119d20fee5578ecdbc4d9007dda25f91fd5ae
Robert Lodder <r.lodder@amsterdam.nl>
2018-12-21 13:37:45 +0100

    OP-508: Add validation that at least one of the questions in the contact for is selected or asked

--- src/pages/ContactForm/index.js
+++ src/pages/ContactForm/index.js
@@ -116,13 +116,15 @@ class ContactForm extends React.Component { // eslint-disable-line react/prefer-
                 <div>
                   <FieldControlWrapper render={TextInput} name="name" display="Uw naam" control={this.contactForm.get('name')} />
                   <FieldControlWrapper render={TextInput} name="email" display="Uw e-mailadres" control={this.contactForm.get('email')} />
-                  { this.contactForm.submitted && this.contactForm.getError('noneSelected') ? <div className="group-error">Selecteer minimaal één van onderstaande vragen</div> : null }
-                  <FieldControlWrapper render={CheckboxInput} name="can_i_have_access" display="Kan ik toegang krijgen tot de data uit dit apparaat?" control={this.contactForm.get('can_i_have_access')} />
-                  <FieldControlWrapper render={CheckboxInput} name="questionInfo" display="Kan ik meer informatie krijgen over de data die uw 'slimme apparaat' (baken, camera, sensor e.d.) verzamelt?" control={this.contactForm.get('can_i_get_more_information')} />
-                  <FieldControlWrapper render={CheckboxInput} name="questionUse" display="Mag ik de verzamelde data eventueel gebruiken?" control={this.contactForm.get('can_i_use_collected_data')} />
-                  <FieldControlWrapper render={CheckboxInput} name="questionPersonalData" display="Registreert uw slimme apparaat ook gegevens over personen?" control={this.contactForm.get('does_the_device_register_personal_data')} />
-                  <FieldControlWrapper render={TextAreaInput} name="comment" display="Andere vraag of opmerking (maximaal 250 tekens):" maxLength={MAX_INPUT_LENGTH} control={this.contactForm.get('comment')} />
-
+                  <div className={`rij mode_input text rij_verplicht ${this.contactForm.submitted && this.contactForm.hasError('noneSelected') ? 'row_ongeldig' : ''}`}>
+                    <label htmlFor="questions">Wat wilt u aan de eigenaar vragen?</label>
+                    { this.contactForm.submitted && this.contactForm.hasError('noneSelected') ? <div className="group-error">Stel minimaal één van onderstaande vragen</div> : null }
+                    <FieldControlWrapper render={CheckboxInput} aria-labelledby="questions" name="can_i_have_access" display="Kan ik toegang krijgen tot de data uit dit apparaat?" control={this.contactForm.get('can_i_have_access')} />
+                    <FieldControlWrapper render={CheckboxInput} aria-labelledby="questions" name="questionInfo" display="Kan ik meer informatie krijgen over de data die uw 'slimme apparaat' (baken, camera, sensor e.d.) verzamelt?" control={this.contactForm.get('can_i_get_more_information')} />
+                    <FieldControlWrapper render={CheckboxInput} aria-labelledby="questions" name="questionUse" display="Mag ik de verzamelde data eventueel gebruiken?" control={this.contactForm.get('can_i_use_collected_data')} />
+                    <FieldControlWrapper render={CheckboxInput} aria-labelledby="questions" name="questionPersonalData" display="Registreert uw slimme apparaat ook gegevens over personen?" control={this.contactForm.get('does_the_device_register_personal_data')} />
+                    <FieldControlWrapper render={TextAreaInput} aria-labelledby="questions" name="comment" display="Andere vraag of opmerking (maximaal 250 tekens):" maxLength={MAX_INPUT_LENGTH} control={this.contactForm.get('comment')} />
+                  </div>
                   <button className="action secundary-blue" type="submit">
                     <span className="value">Versturen</span>
                   </button>
