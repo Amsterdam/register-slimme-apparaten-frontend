5906e967add8bb21e2e107208b8ada19ccf6f790
JJM van Deursen <janjaap@users.noreply.github.com>
2019-03-18 17:30:02 +0100

    Removes page routes

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -5,11 +5,7 @@ import { compose } from 'redux';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
 
-import MapInteractive from 'components/MapInteractive';
-import ContactForm from 'pages/ContactForm';
-import Categories from 'pages/DeviceCategories';
-import About from 'pages/About';
-import FAQ from 'pages/FAQ';
+import Home from 'pages/Home';
 import NotFoundPage from 'containers/NotFoundPage';
 import Footer from 'components/Footer';
 import HeaderContainer from 'containers/HeaderContainer';
@@ -26,11 +22,7 @@ export class App extends React.Component { // eslint-disable-line react/prefer-s
           <div className="row">
             <div className="col-12 col-sm-10 offset-sm-1 col-md-8">
               <Switch>
-                <Route exact path="/" component={MapInteractive} />
-                <Route path="/contact-owner/:deviceId/" component={ContactForm} />
-                <Route path="/categories" component={Categories} />
-                <Route path="/about/faq" component={FAQ} />
-                <Route path="/about" component={About} />
+                <Route exact path="/" component={Home} />
                 <Route path="" component={NotFoundPage} />
               </Switch>
             </div>
