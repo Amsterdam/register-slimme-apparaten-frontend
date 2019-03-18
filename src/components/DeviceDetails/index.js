60e570f03986d9fa6296f6168fb719f9dbc4b087
JJM van Deursen <janjaap@users.noreply.github.com>
2019-03-18 17:30:49 +0100

    Opens modal from DeviceDetauils component

--- src/components/DeviceDetails/index.js
+++ src/components/DeviceDetails/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
 
 import { getMarkerCategory } from '../../services/iotmap';
 
 import CloseIcon from '../../images/icon-cross-big.svg';
 import QuestionMarkIcon from '../../images/icon-question-mark.svg';
 import MailIcon from '../../images/icon-mail.svg';
+import Modal from '../../components/Modal';
+import DeviceCategories from '../Modal/DeviceCategories';
+import withModal from '../withModal';
+import ContactForm from '../Modal/ContactForm';
 
 import './style.scss';
 
@@ -14,67 +17,111 @@ class DeviceDetails extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { isMapPreviewPanelVisible: true };
+    this.state = {
+      contactModalActive: false,
+      isMapPreviewPanelVisible: true,
+      typesModalActive: false,
+    };
+  }
+
+  activateContactModal = () => {
+    this.setState({ contactModalActive: true, typesModalActive: false }, this.props.activateModal());
+  }
+
+  activateTypesModal = () => {
+    this.setState({ contactModalActive: false, typesModalActive: true }, this.props.activateModal());
   }
 
   render() {
-    const ContactButton = (<Route
-      render={({ history }) => (
-        <button className="device-details__contact-button action secundary-blue" onClick={() => { history.push(`/contact-owner/${this.props.device.id}`); }}>
-          <MailIcon />Contact met eigenaar
-        </button>
-      )}
-    />);
+    const { contactModalActive, typesModalActive } = this.state;
+    const { modalActive, device } = this.props;
 
-    const TypesButton = (<Route
-      render={({ history }) => (
-        <button className="device-details__question-mark-button" onClick={() => { history.push('/categories'); }}>
-          <QuestionMarkIcon></QuestionMarkIcon>
-        </button>
-      )}
-    />);
+    const ContactButton = (
+      <button className="device-details__contact-button action secundary-blue" onClick={this.activateContactModal}>
+        <MailIcon />Contact met eigenaar
+      </button>
+    );
+
+    const TypesButton = (
+      <button className="device-details__question-mark-button" onClick={this.activateTypesModal}>
+        <QuestionMarkIcon />
+      </button>
+    );
 
     return (
-      <section id="device-details" className="device-details">
-        <div className="device-details__heading">
-          <button
-            className="device-details__button"
-            onClick={this.props.onDeviceDetailsClose}
-            title="Sluiten"
-          >
-            <CloseIcon className="device-details__button-icon" />
-          </button>
-        </div>
-        <div className="device-details__body">
-          <div className="device-details__table">
-            <div className="device-details__header-row device-details__row">
-              <div className="device-details__row-label">Apparaat</div>
-              <div className="device-details__row-element">{this.props.device.name}</div>
-            </div>
-            <div className="device-details__row">
-              <div className="device-details__row-label">Categorie</div>
-              <div className="device-details__row-element">{getMarkerCategory(this.props.device).name}</div>
-              { TypesButton }
-            </div>
-            { this.props.device.types && <div className="device-details__row">
-              <div className="device-details__row-label">Type</div>
-              <div className="device-details__row-element">{(this.props.device.types.length && this.props.device.types[0].name) || 'Onbekend'}</div>
-            </div> }
+      <Fragment>
+        <section id="device-details" className="device-details">
+          <div className="device-details__heading">
+            <button
+              className="device-details__button"
+              onClick={this.props.onDeviceDetailsClose}
+              title="Sluiten"
+            >
+              <CloseIcon className="device-details__button-icon" />
+            </button>
           </div>
-          {ContactButton}
-        </div>
-      </section>
+
+          <div className="device-details__body">
+            <div className="device-details__table">
+              <div className="device-details__header-row device-details__row">
+                <div className="device-details__row-label">Apparaat</div>
+                <div className="device-details__row-element">{this.props.device.name}</div>
+              </div>
+
+              <div className="device-details__row">
+                <div className="device-details__row-label">Categorie</div>
+                <div className="device-details__row-element">{getMarkerCategory(this.props.device).name}</div>
+                {TypesButton}
+              </div>
+
+              {this.props.device.types && (
+                <div className="device-details__row">
+                  <div className="device-details__row-label">Type</div>
+                  <div className="device-details__row-element">{(this.props.device.types.length && this.props.device.types[0].name) || 'Onbekend'}</div>
+                </div>
+              )}
+            </div>
+
+            {ContactButton}
+          </div>
+        </section>
+
+        {modalActive && contactModalActive && (
+          <Modal
+            id="contact-modal"
+            ariaLabel="Contact met eigenaar"
+          >
+            <ContactForm deviceId={device.id} />
+          </Modal>
+        )}
+
+        {modalActive && typesModalActive && (
+          <Modal
+            id="categories-modal"
+            ariaLabel="Apparaat categorieën"
+          >
+            <DeviceCategories />
+          </Modal>
+        )}
+      </Fragment>
     );
   }
 }
 
 DeviceDetails.propTypes = {
+  activateModal: PropTypes.func.isRequired,
   device: PropTypes.shape({
     id: PropTypes.number,
     types: PropTypes.array.isRequired,
     name: PropTypes.string, // Back-end does not provide value at this time
   }).isRequired,
+  modalActive: PropTypes.bool.isRequired,
   onDeviceDetailsClose: PropTypes.func
 };
 
-export default DeviceDetails;
+const DeviceDetailsWithModal = withModal(DeviceDetails);
+
+export {
+  DeviceDetailsWithModal as default,
+  DeviceDetails as DeviceDetailsComponent,
+};
