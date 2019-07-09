1639fe2023f663a15ebbcbc887796c290d0cb91d
Jefwillems <jefwillems100@gmail.com>
2019-07-09 10:13:22 +0200

    fix review questions

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,18 +4,13 @@ import { NavLink } from 'react-router-dom';
 import '../../../public/images/map-thumbnail.png';
 import './style.scss';
 
-class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div className="header-component no-print">
-        <NavLink className="header-link" to="/">
-          <img className="map-thumbnail" src="/assets/map-thumbnail.png" alt="" />
-          <span className="header-title">{process.env.CITY_NAME}</span>
-        </NavLink>
-      </div>
-    );
-  }
-
-}
+const Header = () => (
+  <div className="header-component no-print">
+    <NavLink className="header-link" to="/">
+      <img className="map-thumbnail" src={process.env.CITY_HEADER_THUMBNAIL} alt="" />
+      <span className="header-title">{process.env.CITY_NAME}</span>
+    </NavLink>
+  </div>
+);
 
 export default Header;
