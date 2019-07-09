634af0d9fe64850b41ab50c2a6ef216d3e81f0f1
Jefwillems <jefwillems100@gmail.com>
2019-07-09 10:27:15 +0200

    undo previous changes

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -123,7 +123,7 @@ module.exports = (options) => ({
 });
 
 const getEnvVariables = () => {
-  const parsed = dotenv.config({ path: process.env === 'production' ? '.env' : '.env.local' }).parsed;
+  const parsed = dotenv.config().parsed;
   const citySpecificEnvs = Object.entries(parsed).reduce((p, [key, value]) => {
     // eslint-disable-next-line no-param-reassign
     p[key] = JSON.stringify(value);
