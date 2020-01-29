51b908f220b9f2f7c733d29e5113e6d68ac6bac5
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-01-29 14:18:07 +0100

    higlights the selected feature

--- src/components/Map/hooks/useHighlight.js
+++ src/components/Map/hooks/useHighlight.js
@@ -1,22 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import 'leaflet.markercluster';
 
-const useHighlight = () => {
-  const [activeMarker, setActiveMarker] = useState();
+export const HIGHLIGHT_CLASS = 'active-element';
 
-  const highlight = ({ sourceTarget }) => {
-    if (activeMarker) {
-      activeMarker._icon.classList.remove('highlight');
+let areaHighlightLayer;
+
+export const removeCurrentHighlight = () => {
+  if (areaHighlightLayer) {
+    const classList = areaHighlightLayer.getElement().classList;
+    if (classList && classList.remove) {
+      classList.remove(HIGHLIGHT_CLASS);
+    }
+  }
+};
+
+const useHighlight = () => {
+  const activeMarker = useRef(null);
+  const activePolygon = useRef(null);
+
+  const clearSelection = () => {
+    if (activeMarker.current) {
+      activeMarker.current._icon.classList.remove('highlight');
     }
 
-    setActiveMarker(sourceTarget);
+    if (activePolygon.current) {
+      const classList = activePolygon.current.getElement().classList;
+      if (classList && classList.remove) {
+        classList.remove(HIGHLIGHT_CLASS);
+      }
+    }
   };
 
-  useEffect(() => {
-    if (activeMarker) activeMarker._icon.classList.add('highlight');
-  }, [activeMarker]);
+  const highlightMarker = marker => {
+    clearSelection();
+    activeMarker.current = marker;
+    activeMarker.current._icon.classList.add('highlight');
+  };
 
-  return highlight;
+  const highlightPolygon = polygon => {
+    clearSelection();
+    activePolygon.current = polygon;
+    const classList = activePolygon.current.getElement().classList;
+    if (classList && classList.add) {
+      classList.add(HIGHLIGHT_CLASS);
+    }
+  };
+
+  return { highlightMarker, highlightPolygon };
 };
 
 export default useHighlight;
