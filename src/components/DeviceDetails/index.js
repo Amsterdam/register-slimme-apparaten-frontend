3072259e1997f03d3ccce46e44612b72fea827d6
Jasper Swart <jasper.g.swart@gmail.com>
2020-03-06 12:53:06 +0100

    DeviceDetails for functional compopnent

--- src/components/DeviceDetails/index.js
+++ src/components/DeviceDetails/index.js
@@ -14,97 +14,88 @@ const StyledButton = styled(Button)`
   margin: ${themeSpacing(2, 6)};
 `;
 
-class DeviceDetails extends React.Component {
-  constructor(props) {
-    super(props);
+const ContactButton = (
+  <Route
+    render={({ history, location }) => (
+      <StyledButton
+        variant="primary"
+        iconLeft={<MailIcon />}
+        onClick={() => {
+          history.push(`/contact-owner/${location.search}`);
+        }}
+      >
+        Contact met eigenaar
+      </StyledButton>
+    )}
+  />
+);
 
-    this.state = {};
-  }
+const TypesButton = (
+  <Route
+    render={({ history }) => (
+      <button
+        type="button"
+        className="device-details__question-mark-button"
+        onClick={() => {
+          history.push('/categories');
+        }}
+      >
+        <QuestionMarkIcon />
+      </button>
+    )}
+  />
+);
 
-  render() {
-    const ContactButton = (
-      <Route
-        render={({ history, location }) => (
-          <StyledButton
-            variant="primary"
-            iconLeft={<MailIcon />}
-            onClick={() => {
-              history.push(`/contact-owner/${location.search}`);
-            }}
-          >
-            Contact met eigenaar
-          </StyledButton>
-        )}
-      />
-    );
-
-    const TypesButton = (
-      <Route
-        render={({ history }) => (
-          <button
-            type="button"
-            className="device-details__question-mark-button"
-            onClick={() => {
-              history.push('/categories');
-            }}
-          >
-            <QuestionMarkIcon />
-          </button>
-        )}
-      />
-    );
-
-    return (
-      <section id="device-details" className="device-details">
-        <div className="device-details__heading">
-          <button
-            type="button"
-            className="device-details__button"
-            onClick={this.props.onDeviceDetailsClose}
-            title="Sluiten"
-          >
-            <CloseIcon className="device-details__button-icon" />
-          </button>
+const DeviceDetails = props => (
+  <section id="device-details" className="device-details">
+    <div className="device-details__heading">
+      <button
+        type="button"
+        className="device-details__button"
+        onClick={props.onDeviceDetailsClose}
+        title="Sluiten"
+      >
+        <CloseIcon className="device-details__button-icon" />
+      </button>
+    </div>
+    <div className="device-details__body">
+      <div className="device-details__table">
+        <div className="device-details__header-row device-details__row">
+          <div className="device-details__row-label">Apparaat</div>
         </div>
-        <div className="device-details__body">
-          <div className="device-details__table">
-            <div className="device-details__header-row device-details__row">
-              <div className="device-details__row-label">Apparaat</div>
-            </div>
-            <div className="device-details__row">
-              <div className="device-details__row-label">Categorie</div>
-              <div className="device-details__row-element">{this.props.device.category}</div>
-              {TypesButton}
-            </div>
-            {this.props.device.soort && (
-              <div className="device-details__row">
-                <div className="device-details__row-label">Type</div>
-                <div className="device-details__row-element">{this.props.device.soort}</div>
-              </div>
-            )}
-            {this.props.device.organisation && (
-              <div className="device-details__row">
-                <div className="device-details__row-label">Organisatie</div>
-                <div className="device-details__row-element">{this.props.device.organisation}</div>
-              </div>
-            )}
-            {this.props.device.privacy && (
-              <div className="device-details__row">
-                <div className="device-details__row-label">Privacyverklaring</div>
-                <div className="device-details__row-element">
-                  <a href={this.props.device.privacy} target="_blank" rel="noopener noreferrer">
-                    {this.props.device.privacy}
-                  </a>
-                </div>
-              </div>
-            )}
+        <div className="device-details__row">
+          <div className="device-details__row-label">Categorie</div>
+          <div className="device-details__row-element">{props.device.category}</div>
+          {TypesButton}
+        </div>
+        {props.device.soort && (
+          <div className="device-details__row">
+            <div className="device-details__row-label">Type</div>
+            <div className="device-details__row-element">{props.device.soort}</div>
           </div>
-          {ContactButton}
-        </div>
-      </section>
-    );
-  }
-}
+        )}
+        {props.device.organisation && (
+          <div className="device-details__row">
+            <div className="device-details__row-label">Organisatie</div>
+            <div className="device-details__row-element">{props.device.organisation}</div>
+          </div>
+        )}
+        {props.device.privacy && (
+          <div className="device-details__row">
+            <div className="device-details__row-label">Privacyverklaring</div>
+            <div className="device-details__row-element">
+              <a href={props.device.privacy} target="_blank" rel="noopener noreferrer">
+                {props.device.privacy}
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+      {ContactButton}
+    </div>
+  </section>
+);
+
 
 DeviceDetails.propTypes = {
   device: PropTypes.shape({
