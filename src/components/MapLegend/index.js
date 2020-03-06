8f372da3a7e59f78f7172d4158435a9863eaf5ea
Jasper Swart <jasper.g.swart@gmail.com>
2020-03-06 15:35:32 +0100

    is now functional comp

--- src/components/MapLegend/index.js
+++ src/components/MapLegend/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { legend } from 'shared/configuration/categories';
@@ -10,57 +10,50 @@ import MapLayersIcon from '../../images/icon-map-layers.svg';
 
 import './style.scss';
 
-class MapLegend extends React.Component {
-  constructor(props) {
-    super(props);
+const MapLegend = ({ onToggleCategory }) => {
+  const [isLegendVisible, setLegendVisible] = useState(window.innerWidth > 576);
 
-    this.state = { isLegendVisible: window.innerWidth > 576 };
-  }
-
-  render() {
-    const { onToggleCategory } = this.props;
-    const checkboxList = Object.entries(legend).map(([id, category]) => (
-      <div key={category.name} className="map-legend__row mb-1">
-        <Checkbox name="check" checked={category.enabled} onChange={() => onToggleCategory(id)} />
-        <span className="map-legend__icon">
-          <img className="map-legend__icon" src={category.iconUrl} alt="" />
-        </span>
-        <span className="map-legend__row-title">{category.name}</span>
-      </div>
-    ));
-
-    return (
-      <section
-        id="map-legend"
-        aria-label={
-          this.state.isLegendVisible
-            ? 'Kaartlagen legenda, Kaartlagen verbergen'
-            : 'Kaartlagen legenda, Kaartlagen tonen'
-        }
-        aria-expanded={this.state.isLegendVisible}
-        className={`
+  return (
+    <section
+      id="map-legend"
+      aria-label={
+        isLegendVisible
+          ? 'Kaartlagen legenda, Kaartlagen verbergen'
+          : 'Kaartlagen legenda, Kaartlagen tonen'
+      }
+      aria-expanded={isLegendVisible}
+      className={`
           map-legend
-          map-legend--${this.state.isLegendVisible ? 'expanded' : 'collapsed'}
+          map-legend--${setLegendVisible ? 'expanded' : 'collapsed'}
         `}
+    >
+      <button
+        type="button"
+        className="map-legend__header"
+        onClick={() => setLegendVisible(!isLegendVisible)}
+        title={isLegendVisible ? 'Kaartlagen verbergen' : 'Kaartlagen tonen'}
       >
-        <button
-          type="button"
-          className="map-legend__header"
-          onClick={() => this.setState({ isLegendVisible: !this.state.isLegendVisible })}
-          title={this.state.isLegendVisible ? 'Kaartlagen verbergen' : 'Kaartlagen tonen'}
-        >
-          <MapLayersIcon className="map-legend__header-icon" />
-          <h4 className="map-legend__header-title">
-            Apparaten
-          </h4>
-          <CollapseIcon className="map-legend__header-icon map-legend__header-icon--expanded" />
-          <ExpandIcon className="map-legend__header-icon map-legend__header-icon--collapsed" />
-        </button>
-        <div className="map-legend__body">{checkboxList}</div>
-      </section>
-    );
-  }
-}
+        <MapLayersIcon className="map-legend__header-icon" />
+        <h4 className="map-legend__header-title">
+          Apparaten
+        </h4>
+        <CollapseIcon className="map-legend__header-icon map-legend__header-icon--expanded" />
+        <ExpandIcon className="map-legend__header-icon map-legend__header-icon--collapsed" />
+      </button>
+      <div className="map-legend__body">
+        {Object.entries(legend).map(([id, category]) => (
+          <div key={category.name} className="map-legend__row mb-1">
+            <Checkbox name="check" checked={category.enabled} onChange={() => onToggleCategory(id)} />
+            <span className="map-legend__icon">
+              <img className="map-legend__icon" src={category.iconUrl} alt="" />
+            </span>
+            <span className="map-legend__row-title">{category.name}</span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 MapLegend.propTypes = {
   onToggleCategory: PropTypes.func,
