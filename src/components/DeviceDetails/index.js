13dfa8a83f19f5fb5bcc572f0aac9321815a60d8
Jasper Swart <jasper.g.swart@gmail.com>
2020-03-06 13:00:58 +0100

    now using useHistory, useLocation

--- src/components/DeviceDetails/index.js
+++ src/components/DeviceDetails/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { Button, themeSpacing } from '@datapunt/asc-ui';
 import styled from '@datapunt/asc-core';
 
@@ -14,87 +14,77 @@ const StyledButton = styled(Button)`
   margin: ${themeSpacing(2, 6)};
 `;
 
-const ContactButton = (
-  <Route
-    render={({ history, location }) => (
-      <StyledButton
-        variant="primary"
-        iconLeft={<MailIcon />}
-        onClick={() => {
-          history.push(`/contact-owner/${location.search}`);
-        }}
-      >
-        Contact met eigenaar
-      </StyledButton>
-    )}
-  />
-);
+const DeviceDetails = props => {
+  const history = useHistory();
+  const location = useLocation();
 
-const TypesButton = (
-  <Route
-    render={({ history }) => (
-      <button
-        type="button"
-        className="device-details__question-mark-button"
-        onClick={() => {
-          history.push('/categories');
-        }}
-      >
-        <QuestionMarkIcon />
-      </button>
-    )}
-  />
-);
-
-const DeviceDetails = props => (
-  <section id="device-details" className="device-details">
-    <div className="device-details__heading">
-      <button
-        type="button"
-        className="device-details__button"
-        onClick={props.onDeviceDetailsClose}
-        title="Sluiten"
-      >
-        <CloseIcon className="device-details__button-icon" />
-      </button>
-    </div>
-    <div className="device-details__body">
-      <div className="device-details__table">
-        <div className="device-details__header-row device-details__row">
-          <div className="device-details__row-label">Apparaat</div>
-        </div>
-        <div className="device-details__row">
-          <div className="device-details__row-label">Categorie</div>
-          <div className="device-details__row-element">{props.device.category}</div>
-          {TypesButton}
-        </div>
-        {props.device.soort && (
-          <div className="device-details__row">
-            <div className="device-details__row-label">Type</div>
-            <div className="device-details__row-element">{props.device.soort}</div>
-          </div>
-        )}
-        {props.device.organisation && (
-          <div className="device-details__row">
-            <div className="device-details__row-label">Organisatie</div>
-            <div className="device-details__row-element">{props.device.organisation}</div>
-          </div>
-        )}
-        {props.device.privacy && (
-          <div className="device-details__row">
-            <div className="device-details__row-label">Privacyverklaring</div>
-            <div className="device-details__row-element">
-              <a href={props.device.privacy} target="_blank" rel="noopener noreferrer">
-                {props.device.privacy}
-              </a>
-            </div>
-          </div>
-        )}
+  return (
+    <section id="device-details" className="device-details">
+      <div className="device-details__heading">
+        <button
+          type="button"
+          className="device-details__button"
+          onClick={props.onDeviceDetailsClose}
+          title="Sluiten"
+        >
+          <CloseIcon className="device-details__button-icon" />
+        </button>
       </div>
-      {ContactButton}
-    </div>
-  </section>
-);
+      <div className="device-details__body">
+        <div className="device-details__table">
+          <div className="device-details__header-row device-details__row">
+            <div className="device-details__row-label">Apparaat</div>
+          </div>
+          <div className="device-details__row">
+            <div className="device-details__row-label">Categorie</div>
+            <div className="device-details__row-element">{props.device.category}</div>
+            <button
+              type="button"
+              className="device-details__question-mark-button"
+              onClick={() => {
+                history.push('/categories');
+              }}
+            >
+              <QuestionMarkIcon />
+            </button>
+
+          </div>
+          {props.device.soort && (
+            <div className="device-details__row">
+              <div className="device-details__row-label">Type</div>
+              <div className="device-details__row-element">{props.device.soort}</div>
+            </div>
+          )}
+          {props.device.organisation && (
+            <div className="device-details__row">
+              <div className="device-details__row-label">Organisatie</div>
+              <div className="device-details__row-element">{props.device.organisation}</div>
+            </div>
+          )}
+          {props.device.privacy && (
+            <div className="device-details__row">
+              <div className="device-details__row-label">Privacyverklaring</div>
+              <div className="device-details__row-element">
+                <a href={props.device.privacy} target="_blank" rel="noopener noreferrer">
+                  {props.device.privacy}
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+        <StyledButton
+          variant="primary"
+          iconLeft={<MailIcon />}
+          onClick={() => {
+            history.push(`/contact-owner/${location.search}`);
+          }}
+        >
+          Contact met eigenaar
+        </StyledButton>
+      </div>
+    </section>
+  );
+};
 
 
 DeviceDetails.propTypes = {
