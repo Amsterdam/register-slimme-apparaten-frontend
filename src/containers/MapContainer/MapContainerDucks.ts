2d8bdc68fd46c6f111e971203d0e2a39a819609e
Jasper Swart <jasper.g.swart@gmail.com>
2021-08-09 12:25:00 +0200

    revert MapContainerDucks

--- src/containers/MapContainer/MapContainerDucks.ts
+++ src/containers/MapContainer/MapContainerDucks.ts
@@ -1,5 +1,5 @@
 import { categories } from '../../shared/configuration/categories';
-import { ActionType, ItemType, LayerType } from '../../utils/types';
+import { ActionType } from '../../utils/types';
 
 // actions
 export const ADD_LAYER_DATA = 'src/containers/MapContainer/ADD_LAYER_DATA';
@@ -8,28 +8,28 @@ export const SELECT_LAYER_ITEM = 'src/containers/MapContainer/SELECT_LAYER_ITEM'
 export const TOGGLE_MAP_LAYER = 'src/containers/MapContainer/TOGGLE_MAP_LAYER';
 
 // action creators
-export function addLayerDataActionCreator(layers: LayerType[]): ActionType {
+export function addLayerDataActionCreator(layers: any[]) {
   return {
     type: ADD_LAYER_DATA,
     payload: layers,
   };
 }
 
-export function removeLayerDataActionCreator(names: string[]): ActionType {
+export function removeLayerDataActionCreator(names: string[]) {
   return {
     type: REMOVE_LAYER_DATA,
     payload: names,
   };
 }
 
-export function selectLayerItemActionCreator(name: string, item?: ItemType): ActionType {
+export function selectLayerItemActionCreator(name?: string, item?: any) {
   return {
     type: SELECT_LAYER_ITEM,
     payload: { name, item },
   };
 }
 
-export function toggleMapLayerActionCreator(name: string): ActionType {
+export function toggleMapLayerActionCreator(name: string) {
   return {
     type: TOGGLE_MAP_LAYER,
     payload: name,
@@ -43,10 +43,9 @@ export const legend = Object.entries(categories).reduce(
 );
 
 export interface MapState {
-  layers: LayerType[] | [];
-  selectedLayer: string | null;
-  selectedItem: ItemType | null;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  layers: Array<any>;
+  selectedLayer: any;
+  selectedItem: any;
   legend: any;
 }
 
@@ -55,10 +54,10 @@ export const initialState: MapState = {
   layers: [],
   selectedLayer: null,
   selectedItem: null,
-  legend: {},
+  legend,
 };
 
-function mapReducer(state = initialState, action: ActionType): MapState {
+function mapReducer(state = initialState, action: ActionType<any>) {
   switch (action.type) {
     case ADD_LAYER_DATA: {
       const result = {
