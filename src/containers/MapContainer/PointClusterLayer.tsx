b75db81811460014a7b1263c9b76b8e15f68cc2b
janthijs <92784122+janthijs@users.noreply.github.com>
2021-11-29 15:26:17 +0100

    fix: remove unused import

--- src/containers/MapContainer/PointClusterLayer.tsx
+++ src/containers/MapContainer/PointClusterLayer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useMapInstance } from '@amsterdam/react-maps';
 import { Feature, FeatureCollection } from 'geojson';
 import L, { DomEvent } from 'leaflet';
