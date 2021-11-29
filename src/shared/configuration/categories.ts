f7c4781d45e791a7df7087ec1fdb91f8cbd5da85
janthijs <92784122+janthijs@users.noreply.github.com>
2021-11-29 17:16:24 +0100

    fix: clean up content

--- src/shared/configuration/categories.ts
+++ src/shared/configuration/categories.ts
@@ -1,13 +1,8 @@
 const ICON_PATH = 'assets/';
 
 export const CATEGORY_NAMES = {
-  CAMERA_TOEZICHTSGEBIED: 'Camera toezichtsgebied',
   CAMERA: 'Camera',
   SENSOR: 'Sensor',
-  BEACONS: 'Baken',
-  SLIMME_VERKEERSINFORMATIE: 'Slimme verkeersinformatie',
-  SLIMME_LAADPAAL: 'Slimme laadpaal',
-  SLIMME_LANTAARNPAAL: 'Slimme lantaarnpaal',
 };
 
 export const DISPLAY_NAMES = {
@@ -16,12 +11,6 @@ export const DISPLAY_NAMES = {
 };
 
 export const categories = {
-  [CATEGORY_NAMES.CAMERA_TOEZICHTSGEBIED]: {
-    iconUrl: `${ICON_PATH}icon-camera-gebied@3x.png`,
-    name: DISPLAY_NAMES.CAMERA_TOEZICHTSGEBIED,
-    enabled: true,
-    visible: true,
-  },
   [CATEGORY_NAMES.CAMERA]: {
     isClustered: false,
     iconUrl: `${ICON_PATH}icon-camera@3x.png`,
@@ -46,34 +35,6 @@ export const categories = {
     wikipediaDescription:
       'In the broadest definition, a sensor is a device, module, or subsystem whose purpose is to detect events or changes in its environment and send the information to other electronics, frequently a computer processor. A sensor is always used with other electronics, whether as simple as a light or as complex as a computer.',
     subtypes: ['Luchtkwaliteit', 'Vervoerstromen (aantal vervoermiddelen)', 'Geluid'],
-    visible: true,
-  },
-  [CATEGORY_NAMES.BEACONS]: {
-    isClustered: false,
-    iconUrl: `${ICON_PATH}icon-beacon@3x.png`,
-    name: DISPLAY_NAMES.BEACONS,
-    enabled: true,
-    description:
-      'Beacons of bakens zijn kleine zenders die een verbinding maken via Bluetooth met smartphones. Zij worden meestal gebruikt om informatie te verschaffen over bijvoorbeeld rijtijden Openbaar Vervoer (hoe lang tot de volgende tram of bus), over routes e.d. Ook worden ze gebruikt voor marketing doeleinden. Er worden boodschappen verstuurd naar de smartphone om je naar een winkel te lokken die in de buurt is.',
-    wikipediaUrl: 'https://en.wikipedia.org/wiki/Bluetooth_low_energy_beacon',
-    wikipediaDescription:
-      'A beacon is an intentionally conspicuous device designed to attract attention to a specific location.',
-    subtypes: [],
-    visible: true,
-  },
-  [CATEGORY_NAMES.SLIMME_VERKEERSINFORMATIE]: {
-    isClustered: false,
-    iconUrl: `${ICON_PATH}icon-verkeer@3x.png`,
-    name: DISPLAY_NAMES.SLIMME_VERKEERSINFORMATIE,
-    enabled: true,
-    description:
-      'De ‘slimme’ systemen voor verkeersinformatie geven informatie op basis van elektronische ‘waarnemingen’, bijvoorbeeld het aantal voertuigen, of het weer. Er zijn bijvoorbeeld stoplichten die langer groen blijven voor fietsen als het regent, of die op groen gaan als een vervoermiddel nadert terwijl de andere wegen geen verkeer hebben. Er zijn nog meer mogelijkheden voor verkeersmanagement, bijvoorbeeld op basis van sensoren en meetpunten in het wegdek en rondom de weg. In het artikel <a href="http://verkeer.wikia.com/wiki/Meetsystemen" target="_blank">http://verkeer.wikia.com/wiki/Meetsystemen</a> worden weggebonden en voertuiggebonden meetsystemen toegelicht. Verkeersgegevens kunnen voor uiteenlopende toepassingen worden gebruikt, zoals <a href="http://verkeer.wikia.com/wiki/Verkeersmonitoring_voor_Dynamisch_Verkeersmanagement" target="_blank">dynamisch verkeersmanagement</a>, <a href="http://verkeer.wikia.com/wiki/Verkeersmonitoring_voor_ontwikkeling_en_evaluatie_van_maatregelen" target="_blank">evaluatie van maatregelen</a>, <a href="http://verkeer.wikia.com/wiki/Verkeersmonitoring_voor_Beleid" target="_blank">opstellen van beleid</a>, <a href="http://verkeer.wikia.com/wiki/Verkeersmonitoring_voor_actuele_verkeersinformatie" target="_blank">verkeersinformatie</a> of <a href="http://verkeer.wikia.com/wiki/Verkeersmonitoring_voor_Onderzoek" target="_blank">onderzoek</a>. Er zijn verschillende monitoringssystemen om gegevens over verkeerssituaties in te winnen.',
-    wikipediaUrl: '',
-    wikipediaDescription: '',
-    subtypes: [
-      'Slimme verkeerslichten: reageert op de omgeving op basis van verzamelde data, bijvoorbeeld over het weer of het aantal vervoermiddelen.',
-      'DRIPs: één van de mogelijkheden om van borden informatie te geven aan verkeersdeelnemers. De informatie op de borden kan aangestuurd worden vanuit een centraal punt en op basis van sensoren en/of bakens. Als dat laatste gebeurt, wordt de DRIP opgenomen in het register.',
-    ],
     visible: true,
   },
 };
