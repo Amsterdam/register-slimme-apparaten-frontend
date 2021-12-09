eb780b851dea7a12324348eec8ec180b15ad4f00
janthijs <92784122+janthijs@users.noreply.github.com>
2021-12-09 12:07:17 +0100

    fix: make new page

---
+++ src/pages/Migration/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Migration = () => (
+  <div>
+    <h2>Slimmeapparten wordt Sensorenregister</h2>
+  </div>
+);
+
+export default Migration;
--- src/services/appRoutes.js
+++ src/services/appRoutes.js
@@ -1,9 +1,9 @@
 const APP_ROUTES = {
   HOME: '/',
-  CONTACT: '/contact-owner',
   CATEGORIES: '/categories',
   ABOUT_FAQ: '/about/faq',
   ABOUT: '/about',
+  MIGRATION: '/slimmeapparaten',
 };
 
 export default APP_ROUTES;
