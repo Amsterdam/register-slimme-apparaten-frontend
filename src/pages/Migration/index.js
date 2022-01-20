58962c8e73977cc8a8d612710c54fc68cdf4dd8c
janthijs <92784122+janthijs@users.noreply.github.com>
2022-01-20 11:39:16 +0100

    fix: add text

--- src/pages/Migration/index.js
+++ src/pages/Migration/index.js
@@ -3,6 +3,11 @@ import React from 'react';
 const Migration = () => (
   <div>
     <h2>Slimmeapparten wordt Sensorenregister</h2>
+    <p>
+      Met ingang van 1 december 2021 moeten sensoren die met een professioneel doel in de openbare ruimte van Amsterdam
+      geïnstalleerd zijn en geschikt zijn om data te verzamelen, gemeld worden bij de gemeente Amsterdam. Andere
+      bewoners of ondernemers kunnen het register inzien.
+    </p>
   </div>
 );
 
